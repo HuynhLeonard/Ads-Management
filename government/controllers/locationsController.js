f2cc247e0e200b6926d0a0cd39f39bc39544b5d5
Leonard <thienhuuhuynh.dev@gmailcom>
2023-12-01 22:56:58 +0700

    Upload controller structure

---
+++ government/controllers/locationsController.js
@@ -0,0 +1,10 @@
+import * as LocationService from '../services/locationService.js';
+
+const controller = {}
+
+// get all location, boards
+controller.findLocationsAllDistricts = async (req,res,next) => {
+
+};
+
+export default controller;
