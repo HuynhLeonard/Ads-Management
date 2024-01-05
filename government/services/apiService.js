1696a784ceb6053cb5db8670dd8154ce5d302e3a
Leonard <thienhuuhuynh.dev@gmailcom>
2024-01-05 14:51:11 +0700

    Update Location Details Query

--- government/services/apiService.js
+++ government/services/apiService.js
@@ -174,7 +174,224 @@ export const getAllLocations = async (districtID, wardID) => {
 };
 
 const getLocationDetail = async (locationID) => {
-
+    const option = [
+        {
+            $match: {
+                locationID: 'LC003'
+            }
+        },
+        {
+            $lookup: {
+                from: 'boards',
+                localField: 'locationID',
+                foreignField: 'locationID',
+                as: 'boards',
+                pipeline: [
+                    {
+                        // report of board
+                        $lookup: {
+                            from: 'reports',
+                            localField: 'boardID',
+                            foreignField: 'objectID',
+                            as: 'reports',
+                            pipeline: [
+                                {
+                                    $lookup: {
+                                        from: 'reporttypes',
+                                        localField:'reportType',
+                                        foreignField:'reportTypeID',
+                                        as: 'reporttype'
+                                    },
+                                },
+                                {
+                                    $unwind: {
+                                        path: "$reporttype",
+                                        preserveNullAndEmptyArrays: true
+                                    }
+                                },
+                                {
+                                    $project: {
+                                        _id: 0,
+                                        reportID: 1,
+                                        reporterName: 1,
+                                        sendTime: 1,
+                                        status: 1,
+                                        reportType: '$reporttype.reportTypeName'
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $lookup: {
+                            as: 'boardtype',
+                            from: 'boardtypes',
+                            foreignField: 'typeID',
+                            localField: 'boardType'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$boardtype',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            boardID: 1,
+                            reports: 1,
+                            boardSize: { $concat: [{ $toString: '$width' }, 'x', { $toString: '$height' }] },
+                            quantity: 1,
+                            boardType: '$boardtype.typeName'
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            // report of place
+            $lookup: {
+                as: 'reports',
+                from: 'reports',
+                foreignField: 'objectID',
+                localField: 'locationID',
+                pipeline: [
+                    {
+                        $lookup: {
+                            as: 'reporttype',
+                            from: 'reporttypes',
+                            foreignField: 'reportTypeID',
+                            localField: 'reportType'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: '$reporttype',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            reportID: 1,
+                            reporterName: 1,
+                            sendTime: 1,
+                            status: 1,
+                            reportType: '$reporttype.reportTypeName'
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            // look for ward
+            $lookup: {
+                from: 'wards',
+                localField: 'wardID',
+                foreignField: 'wardID',
+                as: 'ward',
+                pipeline: [
+                    {
+                        $project: {
+                            _id: 0,
+                            wardID: 1,
+                            wardName: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            // district
+            $lookup: {
+                from: 'districts',
+                localField: 'districtID',
+                foreignField: 'districtID',
+                as: 'district',
+                pipeline: [
+                    {
+                        $project: {
+                            _id: 0,
+                            districtID: 1,
+                            districtName: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            // locationType
+            $lookup: {
+                from: 'locationtypes',
+                localField: 'locationType',
+                foreignField: 'locationTypeID',
+                as: 'locationtype'
+            }
+        },
+        {
+            // adsCategory
+            $lookup: {
+                from: 'adscategories',
+                localField: 'adsForm',
+                foreignField: 'CategoriesID',
+                as: 'adsforms'
+            }
+        },
+        {
+            $unwind: {
+                path: "$district",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $unwind: {
+                path: '$ward',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $unwind: {
+                path: '$locationtype',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $unwind: {
+                path: '$adsforms',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                locationID: 1,
+                locationName: 1,
+                Images: 1,
+                address: {
+                    $concat: ['$address', ', Phường ', '$ward.wardName', ', Quận ', '$district.districtName']
+                },
+                adsFormNme: '$adsforms.CategoriesName',
+                locationTypeName: '$locationtype.locationTypeName',
+                planned: {
+                    $cond: {
+                        if: {
+                            $eq: ['$planned', 1]
+                        },
+                        then: 'Đã quy hoạch',
+                        else: 'Chưa quy hoạch'
+                    }
+                },
+                reports: 1,
+                boards: 1
+            }
+        }
+    ]
+    try {
+        return await Location.aggregate(option);
+    } catch (error) {
+        throw new Error('Error getting details of location.')
+    }
 };
 
 const getListReport = async () => {
