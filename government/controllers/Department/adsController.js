b81281a2e7ee9b14ad389bb87b722e03c5492e5d
Lucas-VTL <votanloc1106@gmail.com>
2024-01-12 00:24:22 +0700

    Update license controller

--- government/controllers/Department/adsController.js
+++ government/controllers/Department/adsController.js
@@ -1,4 +1,4 @@
-import * as locationService from '../../services/locationService.js';
+import * as locationService from "../../services/locationService.js";
 import * as boardService from "../../services/boardService.js";
 import * as wardService from "../../services/wardService.js";
 import * as districtService from "../../services/districtService.js";
@@ -8,31 +8,31 @@ import * as boardTypeService from "../../services/boardTypeService.js";
 
 // 2 type including spot and board
 // show all district in table and choose
-export const show = async (req,res) => {
-    const category = req.quert.category || '';
+export const show = async (req, res) => {
+    const category = req.query.category || "";
     // add table
     let checkboxData = await districtService.getAllDistricts();
     checkboxData = checkboxData.map((district) => `Quận ${district.districtName}`);
     let current = 0;
     try {
-        switch(category) {
+        switch (category) {
             // them diem dat
-            case 'spot':
-                title = '';
+            case "spot":
+                title = "";
                 tableHeads = [];
                 tableData = await locationService.getAllLocation();
                 tableData = tableData.map((location) => {
                     // data for each location
-                })
-                break
-            case 'board':
-                title = '';
+                });
+                break;
+            case "board":
+                title = "";
                 tableHeads = [];
                 tableData = await boardService.getAllBoards();
                 tableData = tableData.map((board) => {
                     // data for each board
-                })
-                break
+                });
+                break;
             default:
                 res.status(404).render();
         }
@@ -44,26 +44,14 @@ export const show = async (req,res) => {
     }
 };
 
-export const showDetail = async (req,res) => {
+export const showDetail = async (req, res) => {};
 
-};
+export const showAddForm = async (req, res) => {};
 
-export const showAddForm = async (req,res) => {
+export const showModifyForm = (req, res) => {};
 
-};
+export const addNewSpot = async (req, res) => {};
 
-export const showModifyForm = (req, res) => {
+export const addNewBoard = async (req, res) => {};
 
-};
-
-export const addNewSpot = async (req,res) => {
-
-};
-
-export const addNewBoard = async (req,res) => {
-
-};
-
-export const performAdd = async (req,res) => {
-
-};
+export const performAdd = async (req, res) => {};
--- government/controllers/WardDistrict/licenseController.js
+++ government/controllers/WardDistrict/licenseController.js
@@ -1,32 +1,34 @@
-import {createLicense, deleteLicenseRequest} from "../../services/licensingService.js";
+import { createLicense, deleteLicenseRequest } from "../../services/licensingService.js";
 import {} from "../../services/locationService.js";
 import {} from "../../services/userService.js";
-import {} from '../../services/boardTypeService.js';
+import {} from "../../services/boardTypeService.js";
 import {} from "../../services/boardService.js";
 import {} from "../../services/districtService.js";
 import {} from "../../services/wardService.js";
 
-const show = (req,res) => {
+const show = (req, res) => {};
 
+const showDetail = (req, res) => {};
+
+const add = async (req, res) => {
+    try {
+        const license = req.body;
+        license.status = 0;
+        await createLicense(license);
+        res.status(200).json({
+            license,
+        });
+    } catch (error) {
+        res.status(500).json("Getting error when add new license request");
+    }
 };
 
-const showDetail = (req,res) => {
-
-};
-
-const add = async (req,res) => {
-	try {
-		// lấy data với chỉnh sửa data
-		
-		// gọi database
-	} catch (error) {
-		res.status(500).json('Message.');
-	}
-};
-
-// http://localhost:3000/district/license/:id
-// http://localhost:3000/district/license?id=BC001
-const deleteRequest = async (req,res) => {
-	// lấy được id
-	const id = req.query.id;
+const deleteRequest = async (req, res) => {
+    try {
+        const id = req.param.id;
+        await deleteLicenseRequest(id);
+        res.status(200).json("Delete license request success");
+    } catch (error) {
+        res.status(500).json("Getting error when delete license request");
+    }
 };
