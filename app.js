50cad3551b884b9cfdf12507c0869db4a7009fa7
Sammm <ntdsam@gmail.com>
2024-01-11 13:38:45 +0700

    update app.js to merge

--- app.js
+++ app.js
@@ -23,44 +23,28 @@ citizenServer.listen(process.env.CITIZENPORT, () => {
 // ======================================================================================
 
 // setup for governmentApp
-import {
-  createCategories,
-  deleteCategorires,
-  getAllCategories,
-  getSingleCategories,
-  modifyCategories,
-} from "./government/services/adsCategoriesService.js";
-import {
-  createNewDistrict,
-  deleteDistrict,
-  getAllDistricts,
-  getDistrictByID,
-  updateDistrict,
-} from "./government/services/districtService.js";
-import {
-  createNewLocation,
-  getAllLocation,
-} from "./government/services/locationService.js";
-import {
-  createNewLocationType,
-  deleteLocationType,
-  getAllLocationType,
-  getLocationType,
-  updateLocationType,
-} from "./government/services/locationTypeService.js";
-import {
-  createNewWard,
-  getAllWard,
-  getWard,
-  getWardOfDistrict,
-  updateWard,
-} from "./government/services/wardService.js";
+// import { createCategories, deleteCategorires, getAllCategories, getSingleCategories, modifyCategories } from './government/services/adsCategoriesService.js';
+// import { createNewDistrict, deleteDistrict, getAllDistricts, getDistrictByID, updateDistrict } from './government/services/districtService.js';
+// import { createNewLocation, getAllLocation } from './government/services/locationService.js';
+// import { createNewLocationType, deleteLocationType, getAllLocationType, getLocationType, updateLocationType } from './government/services/locationTypeService.js';
+// import { createNewWard, getAllWard, getWard, getWardOfDistrict, updateWard } from './government/services/wardService.js';
 // import * as boardTypeService from './government/services/boardTypeService.js';
 // import * as boardService from './government/services/boardService.js';
+import departmentRoute from "./government/routes/departmentRoute.js";
+import * as api from "./government/controllers/MainAPI/main.js";
+import { checkAuth } from "./government/middleware/authMiddleware.js";
+// import testController from "./government/controllers/testController.js";
+import flash from "express-flash";
+import session from "express-session";
+import passportConfig from "./government/config/passport.js";
+import passport from "passport";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 const governmentApp = express();
 governmentApp.use(express.json());
 governmentApp.use(express.urlencoded({ extended: false }));
+governmentApp.use(flash());
 governmentApp.use(cors());
 governmentApp.set("views", path.join(__dirname, "government", "views"));
 governmentApp.set("view engine", "ejs");
@@ -69,7 +53,75 @@ console.log(`${governmentApp.get("views")}`);
 const publicDirectory = path.join(__dirname, "/government/public");
 console.log(publicDirectory);
 governmentApp.use(express.static(publicDirectory));
+governmentApp.use(cookieParser("leonardHuynh"));
 
+passportConfig(passport);
+governmentApp.use(
+  session({
+    secret: "hihihi", // secret key
+    resave: false,
+    saveUninitialized: true,
+    // use local session, session store will be cleared when the server restarts
+  })
+);
+governmentApp.use(passport.initialize());
+governmentApp.use(passport.session());
+
+governmentApp.use((req, res, next) => {
+  res.locals.url = req.originalUrl;
+  res.locals.host = req.get("host");
+  res.locals.protocol = req.protocol;
+  res.locals.message = req.flash();
+  res.locals.username = req.signedCookies.username || "";
+  res.locals.password = req.signedCookies.password || "";
+  next();
+});
+
+// mongodb+srv://thienhuuhuynhdev:thienhuu2003@server.1iqibpx.mongodb.net/Advertisment?retryWrites=true&w=majority
+
+governmentApp.get("/", (req, res) => {
+  res.render("index", {
+    title: "Sở Văn Hóa",
+    username: "",
+    password: "",
+  });
+});
+
+import { loginController } from "./government/controllers/authController.js";
+governmentApp.post("/", loginController);
+// governmentApp.post('/', (req,res) => {
+//     res.redirect('/department')
+// });
+
+governmentApp.use("/department", departmentRoute);
+governmentApp.use("/add", (req, res) => {
+  res.render("add");
+});
+
+governmentApp.get("/show", (req, res) => {
+  res.send(req.user);
+});
+
+import apiRoute from "./government/routes/apiRoutes.js";
+import districtRoute from "./government/routes/districtRoute.js";
+import { setHeaders } from "./government/routes/apiRoutes.js";
+governmentApp.use("/api", setHeaders, apiRoute);
+governmentApp.use("/district", checkAuth, districtRoute);
+// governmentApp.get('/api/location/', (req,res) => {
+//     api
+//         .getAllLocations(req.query.districtID, req.query.wardID)
+//         .then((location) => res.status(200).json(location));
+// });
+
+import userController from "./government/controllers/userController.js";
+governmentApp.post("/test", userController.createUser);
+
+// ==============================================================================
+// test area
+import infoController from "./government/controllers/WardDistrict/infoController.js";
+// governmentApp.get('/district/officer/:username', infoController.getInfo);
+
+governmentApp.use(morgan("dev"));
 mongoose
   .connect(
     "mongodb+srv://thienhuuhuynhdev:thienhuu2003@server.1iqibpx.mongodb.net/Advertisment?retryWrites=true&w=majority"
@@ -77,43 +129,6 @@ mongoose
   .then(() => {
     console.log("Connect to Database");
   });
-
-governmentApp.get("/", (req, res) => {
-  res.render("License");
-});
-
-// for district service
-governmentApp.post("/api/district/create-district", createNewDistrict);
-governmentApp.get("/api/district", getAllDistricts);
-governmentApp.get("/api/district/:id", getDistrictByID);
-governmentApp.put("/api/district/:id", updateDistrict);
-governmentApp.delete("/api/district/:id", deleteDistrict);
-
-// for ward service
-governmentApp.post("/api/ward/create-ward", createNewWard);
-governmentApp.get("/api/ward", getAllWard);
-governmentApp.get("/api/ward/single/:wardID", getWard);
-governmentApp.get("/api/ward/:districtID", getWardOfDistrict);
-governmentApp.put("/api/ward/:wardID", updateWard);
-
-// for spot type service
-governmentApp.post("/api/spot-type/create", createNewLocationType);
-governmentApp.get("/api/spot-type", getAllLocationType);
-governmentApp.get("/api/spot-type/:id", getLocationType);
-governmentApp.put("/api/spot-type/:id", updateLocationType);
-governmentApp.delete("/api/spot-type/:id", deleteLocationType);
-
-// for locations
-governmentApp.post("/api/location/create-location", createNewLocation);
-governmentApp.get("/api/location/", getAllLocation);
-
-// for adscategories
-governmentApp.post("/api/category/create-category", createCategories);
-governmentApp.get("/api/category", getAllCategories);
-governmentApp.get("/api/category/:id", getSingleCategories);
-governmentApp.put("/api/category/:id", modifyCategories);
-governmentApp.delete("/api/category/:id", deleteCategorires);
-
 const governmentServer = http.createServer(governmentApp);
 governmentServer.listen(process.env.GOVERNMENT_PORT, () => {
   console.log(
