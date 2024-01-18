70bca808076fcc7c9308bb222c5a2fe6d00cec4e
Leonard <thienhuuhuynh.dev@gmailcom>
2024-01-19 01:42:50 +0700

    update async function

--- app.js
+++ app.js
@@ -8,28 +8,6 @@ import mongoose from "mongoose";
 const __dirname = path.resolve();
 
 dotenv.config(path.join(__dirname, ".env"));
-
-const citizenApp = express();
-
-citizenApp.use(express.static(path.join(__dirname, "citizen/public")));
-citizenApp.use(express.json());
-citizenApp.get("/");
-
-const citizenServer = http.createServer(citizenApp);
-citizenServer.listen(process.env.CITIZENPORT, () => {
-  // console.log(`Citizen App is running on http://localhost:${process.env.CITIZENPORT}`);
-});
-
-// ======================================================================================
-
-// setup for governmentApp
-// import { createCategories, deleteCategorires, getAllCategories, getSingleCategories, modifyCategories } from './government/services/adsCategoriesService.js';
-// import { createNewDistrict, deleteDistrict, getAllDistricts, getDistrictByID, updateDistrict } from './government/services/districtService.js';
-// import { createNewLocation, getAllLocation } from './government/services/locationService.js';
-// import { createNewLocationType, deleteLocationType, getAllLocationType, getLocationType, updateLocationType } from './government/services/locationTypeService.js';
-// import { createNewWard, getAllWard, getWard, getWardOfDistrict, updateWard } from './government/services/wardService.js';
-// import * as boardTypeService from './government/services/boardTypeService.js';
-// import * as boardService from './government/services/boardService.js';
 import departmentRoute from './government/routes/departmentRoute.js';
 // import testController from "./government/controllers/testController.js";
 import cookieParser from 'cookie-parser';
--- government/routes/districtRoute.js
+++ government/routes/districtRoute.js
@@ -9,21 +9,21 @@ router.get("*", (req, res, next) => {
 });
 
 // route trang chu
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     controller.indexController.show(req,res);
 });
 
 // lay thong tin officer (:username)
-router.get("/officer/:username", (req,res) => controller.infoController.getInfo(req,res));
-router.post('/officer/:username', (req, res) => controller.infoController.updateInfo(req, res));
+router.get("/officer/:username", async (req,res) => controller.infoController.getInfo(req,res));
+router.post('/officer/:username', async (req, res) => controller.infoController.updateInfo(req, res));
 //nhóm report
-router.get("/reports", (req,res) => {
+router.get("/reports", async (req,res) => {
     controller.reportController.show(req,res);
 });
-router.get("/reports/:reportID", (req,res) => {
+router.get("/reports/:reportID", async (req,res) => {
     controller.reportController.showDetail(req,res);
 });
-router.post("/reports/:reportID", (req,res) => {
+router.post("/reports/:reportID", async (req,res) => {
     controller.reportController.updateReport(req,res);
 });
 
@@ -34,21 +34,21 @@ router.delete("/locations/:districtID");
 router.patch("/locations/:districtID");
 
 // nhóm bảng quảng cáo
-router.get("/advertisements", (req,res) => controller.adsController.show(req,res));
+router.get("/advertisements", async (req,res) => controller.adsController.show(req,res));
 // /advertisements/new?category=Location
-router.get("/advertisements/new", (req, res) => controller.adsController.showAdd(req,res));
+router.get("/advertisements/new", async (req, res) => controller.adsController.showAdd(req,res));
 router.post("/advertisements/new");
-router.get("/advertisements/:id", (req,res) => controller.adsController.showDetail(req,res,false));
-router.get("/advertisements/:id/modify", (req,res) => controller.adsController.showDetail(req,res,true));
-router.post("/advertisements/:id", (req,res) => controller.adsController.request(req,res));
+router.get("/advertisements/:id", async (req,res) => controller.adsController.showDetail(req,res,false));
+router.get("/advertisements/:id/modify", async (req,res) => controller.adsController.showDetail(req,res,true));
+router.post("/advertisements/:id", async (req,res) => controller.adsController.request(req,res));
 
 // nhóm giấy phép (license)
-router.get('/license', (req, res) => controller.licenseController.show(req, res));
-router.get('/license/create', (req, res) => controller.licenseController.showCreate(req, res));
-router.get('/license/:id', (req, res) => controller.licenseController.showDetail(req, res, true))
-router.delete('/license/:id', (req, res) => controller.licenseController.deleteRequest(req, res));
-router.post('/license', (req, res) => controller.licenseController.add(req, res));
-router.get('/license/extend/:id', (req, res) => controller.licenseController.showExtend(req, res));
-router.post('/license/extend/:id', (req, res) => controller.licenseController.add(req, res));
+router.get('/license', async (req, res) => controller.licenseController.show(req, res));
+router.get('/license/create', async (req, res) => controller.licenseController.showCreate(req, res));
+router.get('/license/:id', async (req, res) => controller.licenseController.showDetail(req, res, true))
+router.delete('/license/:id', async (req, res) => controller.licenseController.deleteRequest(req, res));
+router.post('/license', async (req, res) => controller.licenseController.add(req, res));
+router.get('/license/extend/:id', async (req, res) => controller.licenseController.showExtend(req, res));
+router.post('/license/extend/:id', async (req, res) => controller.licenseController.add(req, res));
 
 export default router;
